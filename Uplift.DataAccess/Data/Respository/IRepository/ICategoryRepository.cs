8c63d21b315835a74975d5112632b4a1c2a249fb
caleb okogbe <okogbecaleb@gmail.com>
2020-08-18 10:33:19 +0100

    category

--- Uplift.DataAccess/Data/Respository/IRepository/ICategoryRepository.cs
+++ Uplift.DataAccess/Data/Respository/IRepository/ICategoryRepository.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Uplift.Models;
 
 namespace Uplift.DataAccess.Data.Respository.IRepository
 {
-    public interface ICategoryRepository
+    public interface ICategoryRepository : IRepository<Category>
     {
+        IEnumerable<SelectListItem> GetCategoryListForDropDown();
+        void Update(Category category);
     }
 }
--- Uplift.DataAccess/Data/Respository/UnitOfWork.cs
+++ Uplift.DataAccess/Data/Respository/UnitOfWork.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Uplift.DataAccess.Data.Respository.IRepository;
 
 namespace Uplift.DataAccess.Data.Respository
 {
-    class UnitOfWork
+    class UnitOfWork : IUnitOfWork
     {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork( ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
     }
 }
