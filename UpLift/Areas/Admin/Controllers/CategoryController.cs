76e7a067e9c6e7da7b5f00d1491aaf8fb728a582
caleb okogbe <okogbecaleb@gmail.com>
2020-08-26 08:09:05 +0100

    upsert post

--- UpLift/Areas/Admin/Controllers/CategoryController.cs
+++ UpLift/Areas/Admin/Controllers/CategoryController.cs
@@ -36,6 +36,25 @@ namespace UpLift.Areas.Admin.Controllers
             }
             return View(category);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if(category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(category);
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(category);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
 
         #region API CALLS
 
